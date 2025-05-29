f196ec6e511da0bbec3711e8da79ddf7cebac101
Daniel Bauer <daniel.bauer@lamie.io>
2025-05-29 19:41:02 +0200

    feat: Update table

--- app/src/app/speed-table.ts
+++ app/src/app/speed-table.ts
@@ -4,36 +4,50 @@ import { AggregatedSpeedTracks } from './shared/aggregated-speed-tracks.model';
 @Component({
   selector: 'app-speed-table',
   template: `
-    <table class="w-full">
-      <thead>
-        <tr>
-          <th class="p-1 font-medium text-left">Timestamp</th>
-          <th class="p-1 font-medium text-right">Avg Speed (Mbps)</th>
-        </tr>
-      </thead>
+    <div class="border overflow-hidden border-neutral-200 rounded-lg">
+      <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-100">
+          <tr>
+            <th
+              class="px-4 py-2 text-left text-sm font-medium text-gray-700 text-left"
+            >
+              Timestamp
+            </th>
+            <th
+              class="px-4 py-2 text-left text-sm font-medium text-gray-700 text-right"
+            >
+              Avg Speed (Mbps)
+            </th>
+          </tr>
+        </thead>
 
-      <tbody>
-        @for( track of speedTracks(); track $index) {
-        <tr [class.text-red-500]="track.averageSpeed < 10">
-          <td class="p-1 text-left">
-            <div class="text-lg">
-              {{ track.end.toFormat('dd.MM. HH:mm') }}
-            </div>
-            <div class="text-xs text-gray-500">
-              {{ track.start.toFormat('HH:mm') }} -
-              {{ track.end.toFormat('HH:mm') }}
-            </div>
-          </td>
-          <td class="p-1 text-right">
-            <div class="text-lg">{{ track.averageSpeed.toFixed(2) }}</div>
-            <div class="text-xs text-gray-500">
-              {{ track.minSpeed.toFixed(2) }} - {{ track.maxSpeed.toFixed(2) }}
-            </div>
-          </td>
-        </tr>
-        }
-      </tbody>
-    </table>
+        <tbody class="divide-y divide-gray-200">
+          @for( track of speedTracks(); track $index) {
+          <tr
+            [class.text-red-500]="track.averageSpeed < 10"
+            [class.bg-red-50]="track.averageSpeed < 10"
+          >
+            <td class="px-4 py-2 text-left">
+              <div class="text-lg">
+                {{ track.end.toFormat('dd.MM. HH:mm') }}
+              </div>
+              <div class="text-xs text-gray-500">
+                {{ track.start.toFormat('HH:mm') }} -
+                {{ track.end.toFormat('HH:mm') }}
+              </div>
+            </td>
+            <td class="px-4 py-2 text-right">
+              <div class="text-lg">{{ track.averageSpeed.toFixed(2) }}</div>
+              <div class="text-xs text-gray-500">
+                {{ track.minSpeed.toFixed(2) }} -
+                {{ track.maxSpeed.toFixed(2) }}
+              </div>
+            </td>
+          </tr>
+          }
+        </tbody>
+      </table>
+    </div>
   `,
 })
 export class SpeedTable {
