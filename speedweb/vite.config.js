bcaa70c8bc91e316c450b5ce299b3d351582afc2
Daniel Bauer <daniel.bauer@lamie.io>
2025-05-29 14:28:35 +0200

    feat: Copy csv file automatically

--- speedweb/vite.config.js
+++ speedweb/vite.config.js
@@ -1,4 +1,21 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+// /** @type {import('vite').UserConfig} */
+// export default {
+//   base: "internet-speed-tracker/",
+// };
+
+export default defineConfig({
   base: "internet-speed-tracker/",
-};
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "../speed_log.csv",
+          dest: "",
+        },
+      ],
+    }),
+  ],
+});
