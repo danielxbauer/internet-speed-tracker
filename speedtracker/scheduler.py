cb496e12f4e0dd04ca0a5fc8f27d7cd46e6462ce
Daniel Bauer <daniel.bauer@lamie.io>
2025-05-29 15:15:41 +0200

    ci: Adjust pipelines

--- speedtracker/scheduler.py
+++ speedtracker/scheduler.py
@@ -3,14 +3,17 @@ import time
 from auto_commit import auto_commit_and_push
 from tracker import test_speed, log_speed
 
-interval = 1 # minutes
+interval = 10 # minutes
 
-def job():
+def trackJob():
     speed = test_speed()
     log_speed(speed)
+    
+def commitJob():
     auto_commit_and_push()
 
-schedule.every(interval).minutes.do(job)
+schedule.every(interval).minutes.do(trackJob)
+schedule.every(interval).minutes.do(commitJob)
 
 print(f"Started speed tracking every {interval} minutes...")
 while True:
